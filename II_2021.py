b54b1100cb95c40cdfa39392291ad3fb27b39f19
Niko <nheikkil13@gmail.com>
2021-05-03 17:15:05 +0300

    basic structure working

--- II_2021.py
+++ II_2021.py
@@ -23,60 +23,85 @@ a functioning TCP part of the course work with little hassle.
 BUFFERSIZE = 1024
 
 def send_and_receive_tcp(address, port, message):
+    """ TCP communication
+    """
+
     print("You gave arguments: {} {} {}".format(address, port, message))
     # create TCP socket
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # connect socket to given address and port
     tcp_socket.connect((address, port))
-    # python3 sendall() requires bytes like object. encode the message with str.encode() command
+
+    # adds the ending to the message
+    message += "\r\n"
     # send given message to socket
     tcp_socket.sendall(message.encode())
     # receive data from socket
-    # data you received is in bytes format. turn it to string with .decode() command
     msg_recv = tcp_socket.recv(BUFFERSIZE).decode()
     # print received data
-    print(msg_recv)
+    print("RECEIVED MESSAGE:", msg_recv)
+    
     # close the socket
     tcp_socket.close()
+    
     # Get your CID and UDP port from the message
     cid, udp_port = msg_recv.split(" ")[1:]
     udp_port = int(udp_port)
-    # Continue to UDP messaging. You might want to give the function some other parameters like the above mentioned cid and port.
+
+    # Continue to UDP messaging
+    message = "Hello from {}".format(cid)
     send_and_receive_udp(address, udp_port, cid, message)
+
     return
  
  
 def send_and_receive_udp(address, port, cid, message):
-    '''
-    Implement UDP part here.
-    '''
-    print("Sending message", message)
+    """ UDP communication
+    """
+
     # creates udp socket
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # creates packet
-    packet = form_udp_packet(cid, message)
-    # sends packet
-    # sent = 0
-    # while sent < calcsize(FS):
-    udp_socket.sendto(packet, (address, port))
-    # receives packet
-    packet_recv = udp_socket.recv(BUFFERSIZE)
-    # reads the message from the received packet
-    msg_recv = unpack_udp_packet(packet_recv)
-    print(msg_recv)
+    
+    # loop
+    while True:
+        print("SENDING MESSAGE:", message)
+
+        # creates packet
+        packet = form_udp_packet(cid, message)
+        
+        # unpacket = unpack_udp_packet(packet)
+        # print("MY PACKET UNPACKED:", unpacket)
+        
+
+        # sends packet
+        x = udp_socket.sendto(packet, (address, port))
+        
+        print("SENT BYTES:", x)
+        # receives packet
+        packet_recv = udp_socket.recv(BUFFERSIZE)
+        # reads the message from the received packet
+        msg_recv = unpack_udp_packet(packet_recv)
+        print("RECEIVED MESSAGE:", msg_recv)
+
+        # break the loop
+        if "Bye" in msg_recv:
+            break
+        
+        message = reverse_words(msg_recv)
+    
     return
+    
  
 def form_udp_packet(cid, content):
     """ Forms the UDP-packet
     """
     # format string
     FS = "!8s??hh128s"
-    # adds the ending string
-    content += "\r\n"
+    # adds the ending string    ******** MAYBE NOT NEEDED WITH UDP ********
+    # content += "\r\n"
     # calculates the message lenght
     msg_len = len(content)
     # packs the data
-    print(calcsize(FS))
     data = pack(FS, cid.encode(), True, True, 0, msg_len, content.encode())
     return data
 
@@ -87,8 +112,20 @@ def unpack_udp_packet(packet):
     FS = "!8s??hh128s"
     # unpack packet
     cid, ack, eom, dr, cl, content = unpack(FS, packet)
+    # return content without padding
+    return content.decode()[:cl]
+
+def reverse_words(msg):
+    """ Reverses words separated by space.
+    """
+    # creates list from words
+    words = msg.split(" ")
+    # reverses list
+    words.reverse()
+    # makes a string from list
+    msg = " ".join(words)
     
-    return content.decode()
+    return msg
 
  
 def main():
